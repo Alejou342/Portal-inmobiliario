78f003fab67709a4f27892374e422b37e0902ca2
Alejou342 <alejandro.alvarez@cpocket.global>
2024-02-12 09:46:33 -0500

    Fix: Change to .svg

--- src/components/CardSection/index.jsx
+++ src/components/CardSection/index.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 const Index = ({ route, value, title }) => {
   return (
     <div className="p-2" title={title}>
-      <Image src={route} alt="icon.png" width={20} height={20} className='mx-auto cursor-pointer' />
+      <Image src={route} alt="icon.svg" width={20} height={20} className='mx-auto cursor-pointer' />
       <p className='text-sm text-center mt-2 cursor-pointer'>{value}</p>
     </div>
   )
